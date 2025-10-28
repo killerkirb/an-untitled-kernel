27fc400c9b732533f8827c03c46c3ec1bf09bd10
shadeykirb <107715222+killerkirb@users.noreply.github.com>
2025-10-28 19:16:13 +0300

    Change output directory for EFI file to 'builds'

--- B.bat
+++ B.bat
@@ -4,6 +4,7 @@ set /p "nam=Enter file name (without extension):"
 ml64 /nologo %nam%.asm /c /Fo %nam%.obj
 link /nologo /SUBSYSTEM:EFI_APPLICATION /ENTRY:EFI_MAIN /MACHINE:X64 %nam%.obj /OUT:%nam%.efi
 
-copy %nam%.efi drive /Y
+copy %nam%.efi builds /Y
+
 
 @echo on
